45a31e79f90a18b6f797eb7714a70627b35e04c7
noone <ahlijun1116@outlook.com>
2021-07-20 20:20:07 +0800

    fix NettyServerHandler

--- simpleRPC/src/main/java/Netty/server/handler/NettyServerHandler.java
+++ simpleRPC/src/main/java/Netty/server/handler/NettyServerHandler.java
@@ -3,17 +3,38 @@ package Netty.server.handler;
 import Netty.Message.RPCMessage;
 import Netty.Message.RPCRequest;
 import Netty.Message.RPCResponse;
+import Netty.Tools.Factory.SingletonFactory;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    private final RpcRequestHandler rpcRequestHandler;
+    public NettyServerHandler(){
+        rpcRequestHandler = SingletonFactory.getInstance(RpcRequestHandler.class);
+    }
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if(msg instanceof RPCMessage){
-            log.info("server receive msg:[{}]",msg);
-            RPCRequest rpcRequest = (RPCRequest) ((RPCMessage) msg).getData();
+        try {
+            if(msg instanceof RPCMessage){
+                log.info("server receive msg:[{}]",msg);
+                RPCMessage rpcMessage = new RPCMessage();
+                RPCRequest rpcRequest = (RPCRequest) ((RPCMessage) msg).getData();
+                Object result = rpcRequestHandler.handle(rpcRequest);
+                RPCResponse rpcResponse = new RPCResponse(rpcRequest.getRequestID(),1,"success",result);
+                rpcMessage.setData(rpcResponse);
+                ctx.writeAndFlush(rpcMessage).addListener(future -> {
+                    if(future.isSuccess()){
+                        log.info("server send response");
+                    }else {
+                        log.error("server send response failed");
+                    }
+                });
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
         }
 
     }
